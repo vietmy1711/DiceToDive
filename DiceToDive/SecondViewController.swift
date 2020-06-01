adca59ae787c54969663b085afe851c78629d3e2
Nguyen Viet My <zzvietmyzz@gmail.com>
2020-06-01 20:59:37 +0700

    Initial Commit

---
+++ DiceToDive/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  DiceToDive
+//
+//  Created by MM on 6/1/20.
+//  Copyright © 2020 MM. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
